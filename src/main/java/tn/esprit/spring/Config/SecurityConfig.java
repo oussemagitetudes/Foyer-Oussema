ecb30b11647f18e7e9f344ab6ab61a26ca4734cf
mliki oussema <omliki@iobeya.com>
2025-07-18 20:04:08 +0100

    Intégration Keycloak Ajouter les dépendances dans pom.xml Configurer la sécurité (application.properties/yml, SecurityConfig) Mapping des rôles et RBAC Sécuriser les endpoints REST

---
+++ src/main/java/tn/esprit/spring/Config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package tn.esprit.spring.Config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@KeycloakConfiguration
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+            .authorizeRequests()
+            .antMatchers("/actuator/**").permitAll() // Expose actuator endpoints
+            .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Expose Swagger
+            .antMatchers("/reservation/**").hasAnyRole("user", "admin")
+            .antMatchers("/etudiant/**").hasRole("admin")
+            .antMatchers("/bloc/**").hasRole("admin")
+            .antMatchers("/foyer/**").hasRole("admin")
+            .antMatchers("/universite/**").hasRole("admin")
+            .anyRequest().authenticated();
+    }
+} 
